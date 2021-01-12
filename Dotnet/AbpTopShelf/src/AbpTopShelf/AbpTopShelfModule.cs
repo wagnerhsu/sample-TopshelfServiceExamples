153abaf1ca6280ded75f0a5f37ec32127a01e9e7
Wagner Xu <wagnerhsu@hotmail.com>
2021-01-12 09:08:00 +0800

    Add AbpTopShelf

---
+++ Dotnet/AbpTopShelf/src/AbpTopShelf/AbpTopShelfModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace AbpTopShelf
+{
+
+    [DependsOn(
+        typeof(AbpAutofacModule)
+    )]
+    public class AbpTopShelfModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            var hostEnvironment = context.Services.GetSingletonInstance<IHostEnvironment>();
+
+            context.Services.AddHostedService<AbpTopShelfHostedService>();
+        }
+    }
+}
---
+++ Dotnet/AbpTopShelf/src/AbpTopShelf/HelloWorldService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Volo.Abp.DependencyInjection;
+
+namespace AbpTopShelf
+{
+    public class HelloWorldService : ITransientDependency
+    {
+        public void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
