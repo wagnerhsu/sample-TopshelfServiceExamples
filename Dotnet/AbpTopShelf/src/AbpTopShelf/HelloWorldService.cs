af28024e6ea4bb3b71905685c26f598e62501e4f
Wagner Xu <wagnerhsu@hotmail.com>
2021-01-12 10:13:23 +0800

    Update HelloWorldService

--- Dotnet/AbpTopShelf/src/AbpTopShelf/HelloWorldService.cs
+++ Dotnet/AbpTopShelf/src/AbpTopShelf/HelloWorldService.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using Volo.Abp.DependencyInjection;
 
 namespace AbpTopShelf
 {
     public class HelloWorldService : ITransientDependency
     {
+        private readonly ILogger<HelloWorldService> _logger;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger = logger;
+        }
+
         public void SayHello()
         {
-            Console.WriteLine("Hello World!");
+            _logger.LogInformation("Hello World!");
         }
     }
 }
